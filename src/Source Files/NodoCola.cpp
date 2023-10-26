06fb96627b231906e31d588260df953f2b4fbf17
RodrigoIslan <98980083+RodrigoIslan@users.noreply.github.com>
2023-10-26 22:41:36 +0200

    Add files via upload

--- src/Source Files/NodoCola.cpp
+++ src/Source Files/NodoCola.cpp
@@ -1,6 +1,36 @@
+/*#include "NodoCola.hpp"
+
+NodoCola::NodoCola(Pedido* p, NodoCola* sig)
+{
+    pedido = p;
+    siguiente = sig;
+}
+Pedido* NodoCola::getPedido() { 
+    return pedido; 
+}
+
+NodoCola* NodoCola::getSiguiente() {
+    return siguiente; 
+}
+NodoCola::~NodoCola()
+{
+}*/
 #include "NodoCola.hpp"
 
-NodoCola::NodoCola(const Pedido& p) : pedido(p), siguiente(nullptr) {}
+NodoCola::NodoCola(Pedido* pedido) {
+    this->pedido = pedido;
+    this->siguiente = nullptr;
+}
 
+Pedido* NodoCola::obtenerPedido() {
+    return pedido;
+}
 
+NodoCola* NodoCola::obtenerSiguiente() {
+    return siguiente;
+}
+
+void NodoCola::establecerSiguiente(NodoCola* siguiente) {
+    this->siguiente = siguiente;
+}
 
