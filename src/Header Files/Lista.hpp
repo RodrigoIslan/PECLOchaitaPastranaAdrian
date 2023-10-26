fbde96266dd34fd7a63c743b53ba6d503996481f
RodrigoIslan <98980083+RodrigoIslan@users.noreply.github.com>
2023-10-26 22:39:34 +0200

    Add files via upload

--- src/Header Files/Lista.hpp
+++ src/Header Files/Lista.hpp
@@ -1,12 +1,24 @@
 #ifndef LISTA_HPP
 #define LISTA_HPP
-
-class Lista
-{
+#include "Pedido.hpp"
+#include "NodoLista.hpp"
+#include <list>
+class Lista {
 public:
     Lista();
     ~Lista();
-
+    void insertarEnOrden(Pedido* pedido);
+    void mostrarPedidos(Lista& lista);
+    bool estaVacia();
+    Pedido* pop();
+    void push(Pedido* pedido);
+    Pedido* encontrarPedidoUrgenteMenosPrioritario();
+    Pedido* encontrarPedidoEstandarPrioritario();
+    void limpiar();
+    int longitud();
+private:
+    NodoLista* cima;
+    NodoLista* fin;
+    //std::list<Pedido*> lista;
 };
-
 #endif // LISTA_HPP
